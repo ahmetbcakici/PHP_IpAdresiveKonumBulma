72719adab1828a77968a23726d2d24f4996ab881
ahmetbcakici <ahmetbcakici@gmail.com>
2019-08-28 23:13:53 +0300

    initial

---
+++ ipaddress.php
@@ -0,0 +1,30 @@
+<?php
+$ip = get_client_ip();
+
+$url = "http://ip-api.com/json/".$ip;
+$content = file_get_contents($url);
+$json = json_decode($content,true);
+echo $json["city"];
+echo $json["country"];
+
+function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+
+
+?>
